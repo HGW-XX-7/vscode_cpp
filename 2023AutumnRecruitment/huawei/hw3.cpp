05b6208bc2dea5ccfbcd3baacd6467527d79dd60
xqjauto <xqjauto@163.com>
2023-09-20 21:21:23 +0800

    hw

--- 2023AutumnRecruitment/huawei/hw3.cpp
+++ 2023AutumnRecruitment/huawei/hw3.cpp
@@ -26,69 +26,6 @@ bool isValidVariableName(const string &name)
     return true;
 }
 
-int main()
-{
-    string line;
-    while (getline(cin, line))
-    {
-        if (line.empty())
-        {
-            continue;
-        }
-        size_t letPos = line.find("let ");
-        if (letPos != string::npos)
-        {
-            size_t equalsPos = line.find('=');
-            if (equalsPos != string::npos)
-            {
-                string assignment = line.substr(letPos + 4, equalsPos - letPos - 4);
-                stringstream ss(assignment);
-                string varName, expr;
-                ss >> varName >> expr;
-                if (isValidVariableName(varName))
-                {
-                    variables[varName] = calculate(expr);
-                }
-                else
-                {
-                    cout << "Invalid variable name." << endl;
-                }
-            }
-            else
-            {
-                cout << "Invalid assignment format." << endl;
-            }
-        }
-        else if (line.find("out(") != string::npos)
-        {
-            size_t startPos = line.find("out(");
-            size_t endPos = line.find(")");
-            if (startPos != string::npos && endPos != string::npos)
-            {
-                string varName = line.substr(startPos + 4, endPos - startPos - 4);
-                if (variables.find(varName) != variables.end())
-                {
-                    cout << variables[varName] << endl;
-                }
-                else
-                {
-                    cout << "Variable is undefined." << endl;
-                }
-            }
-            else
-            {
-                cout << "Invalid out function format." << endl;
-            }
-        }
-        else
-        {
-            cout << "Invalid input." << endl;
-        }
-    }
-
-    return 0;
-}
-
 int calculate(string expression)
 {
     // 简单的四则运算表达式求值
@@ -160,4 +97,67 @@ int calculate(string expression)
     }
 
     return result;
+}
+
+int main()
+{
+    string line;
+    while (getline(cin, line))
+    {
+        if (line.empty())
+        {
+            continue;
+        }
+        size_t letPos = line.find("let ");
+        if (letPos != string::npos)
+        {
+            size_t equalsPos = line.find('=');
+            if (equalsPos != string::npos)
+            {
+                string assignment = line.substr(letPos + 4, equalsPos - letPos - 4);
+                stringstream ss(assignment);
+                string varName, expr;
+                ss >> varName >> expr;
+                if (isValidVariableName(varName))
+                {
+                    variables[varName] = calculate(expr);
+                }
+                else
+                {
+                    cout << "<undefined>" << endl;
+                }
+            }
+            else
+            {
+                cout << "<syntax-error>" << endl;
+            }
+        }
+        else if (line.find("out(") != string::npos)
+        {
+            size_t startPos = line.find("out(");
+            size_t endPos = line.find(")");
+            if (startPos != string::npos && endPos != string::npos)
+            {
+                string varName = line.substr(startPos + 4, endPos - startPos - 4);
+                if (variables.find(varName) != variables.end())
+                {
+                    cout << variables[varName] << endl;
+                }
+                else
+                {
+                    cout << "<undefined>" << endl;
+                }
+            }
+            else
+            {
+                cout << "<overflow>" << endl;
+            }
+        }
+        else
+        {
+            cout << "Invalid input." << endl;
+        }
+    }
+
+    return 0;
 }
