f00a97866ab44dc523a7efde7573c04d96d16cea
HGW XX/7 <xqjauto@163.com>
2023-09-23 23:30:33 +0800

    取消了大括号换行

--- readArr.cpp
+++ readArr.cpp
@@ -9,35 +9,27 @@ vector<vector<int>> StringToVector(string str)
 {
     vector<vector<int>> result;
     int i = 0;
-    while (i < str.size())
-    {
-        if (str[i] == '[')
-        {
-            if (str[i + 1] == '[')
-            {
+    while (i < str.size()) {
+        if (str[i] == '[') {
+            if (str[i + 1] == '[') {
                 i++;
                 continue;
             }
             vector<int> row;
             i++;
-            while (i < str.size() && str[i] != ']')
-            {
-                if (str[i] == ',')
-                {
+            while (i < str.size() && str[i] != ']') {
+                if (str[i] == ',') {
                     i++;
                 }
                 int num = 0;
-                while (i < str.size() && str[i] >= '0' && str[i] <= '9')
-                {
+                while (i < str.size() && str[i] >= '0' && str[i] <= '9') {
                     num = num * 10 + (str[i] - '0');
                     i++;
                 }
                 row.push_back(num);
             }
             result.push_back(row);
-        }
-        else
-        {
+        } else {
             i++;
         }
     }
@@ -49,10 +41,8 @@ int main()
     string input;
     getline(cin, input);
     vector<vector<int>> arr = StringToVector(input);
-    for (int i = 0; i < arr.size(); i++)
-    {
-        for (int j = 0; j < arr[i].size(); j++)
-        {
+    for (int i = 0; i < arr.size(); i++) {
+        for (int j = 0; j < arr[i].size(); j++) {
             cout << arr[i][j] << " ";
         }
         cout << endl;
